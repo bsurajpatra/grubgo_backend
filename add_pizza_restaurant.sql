d7088a9c54794a7aeb0317bc63b5f1a8f413b056
bsurajpatra <ankitsuraj1111@gmail.com>
2025-05-11 06:07:06 +0530

    sdfz

--- add_pizza_restaurant.sql
+++ add_pizza_restaurant.sql
@@ -1,6 +1,6 @@
 -- SQL script to add a restaurant for pizza@example.com user (ID: 18)
 INSERT INTO restaurants (name, address, phone, cuisine_type, opening_hours, owner_id, rating)
 VALUES ('Pizza Palace', '123 Pizza St', '555-0101', 'Italian', '10:00-22:00', 18, 4.5);
-
+ 
 -- Verify the insertion
 SELECT * FROM restaurants WHERE owner_id = 18; 
